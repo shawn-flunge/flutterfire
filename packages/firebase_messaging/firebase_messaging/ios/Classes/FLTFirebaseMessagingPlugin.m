c5786329f0106c73035f6173039e89c2b93c4e21
shawn <shawn@villagepeople.co.kr>
2022-01-04 15:02:39 +0900

    add invokeMethod

--- packages/firebase_messaging/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
+++ packages/firebase_messaging/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
@@ -145,9 +145,7 @@ static BOOL isBackgroundRunning;
   } else if ([@"Messaging#askIsSelected" isEqualToString:call.method]) {
       
     NSLog(@"FLTFirebaseMessaging: called Messaging#askIsSelected %@", (isBackgroundRunning ? @"YES" : @"NO"));
-//      [_channel invokeMethod:@"Messaging#answerIsSelected"];
-      
-//      [_channel invokeMethod:@"Messaging#answerIsSelected" arguments:isBackgroundRunning : YES ? NO];
+    [_channel invokeMethod:@"Messaging#answerIsSelected" arguments:(isBackgroundRunning ? @"YES" : @"NO")];
     methodCallResult.success(nil);
   } else if ([@"Messaging#setIsSelectedTrue" isEqualToString:call.method]) {
     NSLog(@"FLTFirebaseMessaging: called Messaging#setIsSelectedTrue");
